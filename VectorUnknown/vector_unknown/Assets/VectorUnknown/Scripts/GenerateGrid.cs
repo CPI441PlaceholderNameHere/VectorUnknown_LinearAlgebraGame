61cadc78ec063926ce8a7b943a675b40aec70543
Collin Christensen <collinrchristensen@gmail.com>
2018-02-04 15:23:23 -0700

    added gridtiles generator

--- VectorUnknown/vector_unknown/Assets/VectorUnknown/Scripts/GenerateGrid.cs
+++ VectorUnknown/vector_unknown/Assets/VectorUnknown/Scripts/GenerateGrid.cs
@@ -6,8 +6,15 @@ using UnityEngine;
 
 public class GenerateGrid : MonoBehaviour {
 
-    public int rows = 5;
-    public int columns = 5;
+    public int rows = 20;
+    public int columns;
+
+    public int tileSize = 1;
+    public float offset = 0.5f;
+
+    public int quadrantSize = 10;
+
+    public float gridHeight = 0.1f;
 
     public char[,] tileMapped;
     public GameObject[,] tileGrid;
@@ -22,11 +29,16 @@ public class GenerateGrid : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        columns = rows;
+        offset = tileSize / 2;
+        quadrantSize = rows / 2;
+
         tileMapped = new char[rows, columns];
         ReadTextLevels();
         InitializeGrid();
 
-        mainCamera.transform.position = new Vector3(rows/2,rows,columns/2);
+
+        //mainCamera.transform.position = new Vector3(rows/2,rows,columns/2);
 	}
 	
 	// Update is called once per frame
@@ -38,12 +50,120 @@ public class GenerateGrid : MonoBehaviour {
     {
         gridHolder = new GameObject("grid").transform;
 
-        for (int i = 0; i < rows; i++)
+        offset = (float)tileSize;
+        offset = offset / 2;
+
+        // first quadrant
+        for (int i = quadrantSize; i < rows; i++)
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = quadrantSize; j < columns; j++)
             { 
                 GameObject instance = Instantiate(tile, 
-                    new Vector3(i, 0f, j), Quaternion.identity) as GameObject;
+                    new Vector3(i-quadrantSize, gridHeight, j-quadrantSize), Quaternion.identity) as GameObject;
+
+                instance.transform.Translate(new Vector3(offset, 0.0f, offset));
+
+                instance.transform.SetParent(gridHolder);
+
+                TileMouseOver tileData = instance.GetComponent<TileMouseOver>();
+                char type = tileMapped[i, j];
+
+                tileData.tileType = type;
+
+                // setting tile colors
+                if (tileData.tileType == '0')
+                {
+                    tileData.baseColor = Color.black;
+                }
+                else if (tileData.tileType == 't')
+                {
+                    tileData.baseColor = Color.gray;
+                }
+                else if (tileData.tileType == 'g')
+                {
+                    tileData.baseColor = Color.green;
+                }
+
+            }
+        }
+
+        // second quadrant
+        for (int i = 0; i < quadrantSize; i++)
+        {
+            for (int j = quadrantSize; j < columns; j++)
+            {
+                GameObject instance = Instantiate(tile,
+                    new Vector3(i - quadrantSize, gridHeight, j - quadrantSize), Quaternion.identity) as GameObject;
+
+                instance.transform.Translate(new Vector3(offset, 0.0f, offset));
+
+                instance.transform.SetParent(gridHolder);
+
+                TileMouseOver tileData = instance.GetComponent<TileMouseOver>();
+                char type = tileMapped[i, j];
+
+                tileData.tileType = type;
+
+                // setting tile colors
+                if (tileData.tileType == '0')
+                {
+                    tileData.baseColor = Color.black;
+                }
+                else if (tileData.tileType == 't')
+                {
+                    tileData.baseColor = Color.gray;
+                }
+                else if (tileData.tileType == 'g')
+                {
+                    tileData.baseColor = Color.green;
+                }
+
+            }
+        }
+
+        // third quadrant
+        for (int i = 0; i < quadrantSize; i++)
+        {
+            for (int j = 0; j < quadrantSize; j++)
+            {
+                GameObject instance = Instantiate(tile,
+                    new Vector3(i - quadrantSize, gridHeight, j - quadrantSize), Quaternion.identity) as GameObject;
+
+                instance.transform.Translate(new Vector3(offset, 0.0f, offset));
+
+                instance.transform.SetParent(gridHolder);
+
+                TileMouseOver tileData = instance.GetComponent<TileMouseOver>();
+                char type = tileMapped[i, j];
+
+                tileData.tileType = type;
+
+                // setting tile colors
+                if (tileData.tileType == '0')
+                {
+                    tileData.baseColor = Color.black;
+                }
+                else if (tileData.tileType == 't')
+                {
+                    tileData.baseColor = Color.gray;
+                }
+                else if (tileData.tileType == 'g')
+                {
+                    tileData.baseColor = Color.green;
+                }
+
+            }
+        }
+
+        // fourth quadrant
+        for (int i = quadrantSize; i < rows; i++)
+        {
+            for (int j = 0; j < quadrantSize; j++)
+            {
+                GameObject instance = Instantiate(tile,
+                    new Vector3(i - quadrantSize, gridHeight, j - quadrantSize), Quaternion.identity) as GameObject;
+
+                instance.transform.Translate(new Vector3(offset, 0.0f, offset));
 
                 instance.transform.SetParent(gridHolder);
 
