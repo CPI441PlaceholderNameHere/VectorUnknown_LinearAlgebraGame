65464ffd8bfabbdae9fbef437626ba9dfe43fc3d
Nate <me@natecortes.com>
2018-02-06 15:39:48 -0700

    smooth camera + UI

--- Camera/Assets/VectorUnkown/scripts/rotate_camera.cs
+++ Camera/Assets/VectorUnkown/scripts/rotate_camera.cs
@@ -18,6 +18,8 @@ public class rotate_camera : MonoBehaviour {
         target = GameObject.FindGameObjectWithTag("Center").transform.position;
 		follow = GameObject.FindGameObjectWithTag ("Follow");
 
+
+		transform.RotateAround(target, transform.TransformDirection(Vector3.right), 36.0f);
 		follow.transform.position = transform.position;
 		follow.transform.LookAt(target);
         transform.LookAt(target);
@@ -31,18 +33,22 @@ public class rotate_camera : MonoBehaviour {
             follow.transform.RotateAround(target, Vector3.down, 45.0f);
 		//vetrical rotations
         if (Input.GetKeyDown(KeyCode.UpArrow) && v_lock < 36.0f)
-        {
-            follow.transform.RotateAround(target, transform.TransformDirection(Vector3.right), 12.0f);
-            v_lock += 12.0f;
-        }
+			spin_vertical (12.0f, 1);
         if (Input.GetKeyDown(KeyCode.DownArrow) && v_lock > 0.0f)
-        {
-            follow.transform.RotateAround(target, transform.TransformDirection(Vector3.left), 12.0f);
-            v_lock -= 12.0f;
-        }
+			spin_vertical (12.0f, -1);
+		
 		//SmoothDamp to move the camera to the follow position, then retarget the center of the board
 		transform.position = Vector3.SmoothDamp (transform.position, follow.transform.position, ref velocity, 0.3f);
 		transform.LookAt (target);
     }
 
+	void spin_vertical( float degree, int direction){
+		if (direction > 0) { // if positive, rotate up
+			follow.transform.RotateAround(target, transform.TransformDirection(Vector3.right), degree);
+			v_lock += degree;
+		} else { // if negative, rotate down
+			follow.transform.RotateAround(target, transform.TransformDirection(Vector3.left), degree);
+			v_lock -= degree;
+		}
+	}
 }
--- Camera/Assets/VectorUnkown/scripts/top_down_third.cs
+++ Camera/Assets/VectorUnkown/scripts/top_down_third.cs
@@ -5,11 +5,33 @@ using UnityEngine;
 public class top_down_third : MonoBehaviour {
 
     [SerializeField]
-    private Vector3 center;
+    private Vector3 center;//Center GameObject
+	/*private GameObject follow;//transform to smoothly follow
+	private Vector3 velocity = Vector3.zero;//camera follow velocity
+	public float smooth = 0.3f, distance = -20.0f, yVelocity = 0.0f;*/
 
 	void Start () {
         center = GameObject.FindWithTag("Center").transform.position;
-        Camera.main.transform.position = center + new Vector3(0, 20, 0);
-        Camera.main.transform.LookAt(center);
+		//follow = GameObject.FindWithTag ("Follow1");
+
+		transform.position = center + new Vector3(0, 20, 0);
+		//follow.transform.position = transform.position;
+		//follow.transform.LookAt(center);
+        transform.LookAt(center);
+	}
+
+	void Update(){
+
+		/*if (Input.GetKeyDown(KeyCode.LeftArrow))
+			follow.transform.RotateAround(center, Vector3.up, 45.0f);
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+			follow.transform.RotateAround(center, Vector3.down, 45.0f);
+
+		float angle = Mathf.SmoothDampAngle (transform.eulerAngles.z, follow.transform.eulerAngles.z, ref yVelocity, smooth);
+		Vector3 position = follow.transform.position;
+		position += Quaternion.Euler (0, 0, angle) * new Vector3(0,0,distance);
+		transform.position = position;
+		//transform.position = Vector3.SmoothDamp (transform.position, follow.transform.position, ref velocity, 0.3f);
+		transform.LookAt ( center);*/
 	}
 }
